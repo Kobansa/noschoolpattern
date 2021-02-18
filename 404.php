8b6035a7569b80a2116d61ee53f5eae97f1aadeb
rinakoisaka <33512159+rinakoisaka@users.noreply.github.com>
2021-02-18 23:30:41 +0900

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div id="content">
+
+  <section>
+    <div class="box01">
+      <ul class="pankuzu box02">
+        <li><a href="<?php echo esc_url(home_url('/')); ?>">HOME</a></li>&gt;
+        <li><?php the_title(); ?></li>
+      </ul>
+    </div>
+  </section>
+
+  <section>
+    <div id="" class="box04">
+      <p>ページがありません</p>
+    </div>
+  </section>
+
+
+</div>
+<!-- content -->
+
+<?php
+get_footer(); ?>
