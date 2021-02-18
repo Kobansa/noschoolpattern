cb353f4fc8c7569982a7b07302e05e9e1904d282
rinakoisaka <33512159+rinakoisaka@users.noreply.github.com>
2021-02-18 23:31:42 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,25 @@
+  </div>
+  <!-- wrap -->
+
+  <footer>
+
+  	<div id="footer">
+  		<?php if(is_mobile()) { ?>
+  		<div id="twiiter-boxsp" class="sp">
+         <a class="twitter-timeline" width="260px" height="400px" href="https://twitter.com/noschoolpattern?ref_src=twsrc%5Etfw">Tweets by n学校に行かないパターン</a>
+         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+      </div>
+      <!-- twiiter-box -->
+      <p class="f-copyright sp">Copyright©<br>パラソル All Rights Reserved.</p>
+      <?php } ?>
+		
+			<!--下に見出し-->
+	
+
+  	</div>
+  	<!-- footer -->
+  </footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
